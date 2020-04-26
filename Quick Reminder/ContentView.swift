28e408340a3fe236fa9e4e55bc15eba7876daf22
Kilian Friedrich <kilianfriedrich@icloud.com>
2020-04-26 23:03:09 +0200

    Initial Commit

---
+++ Quick Reminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quick Reminder
+//
+//  Created by Kilian Friedrich on 26.04.20.
+//  Copyright © 2020 Kilian Friedrich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
