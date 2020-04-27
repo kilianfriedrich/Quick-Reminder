c72dc71d921448cb9993085e66eb8a2dc1e35fe1
Kilian Friedrich <kilianfriedrich@icloud.com>
2020-04-27 16:38:45 +0200

    UI implementation

--- Quick Reminder/ContentView.swift
+++ Quick Reminder/ContentView.swift
@@ -8,9 +8,55 @@
 
 import SwiftUI
 
+func notification(_ txt: String) {
+    
+}
+
 struct ContentView: View {
+    
+    @State var txt: String = "Test"
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        let txtBinding: Binding<String> = Binding<String>(
+            get: { self.txt },
+            set: { let newStr = $0; withAnimation() { self.txt = newStr } })
+        
+        return VStack() {
+            
+            Spacer()
+            
+            Text("Quick Reminder")
+                .font(.title)
+            Text("by Kilian Friedrich")
+                .font(.subheadline)
+            
+            Spacer()
+            
+            VStack {
+                
+                TextField("Remind me of...", text: txtBinding)
+                    .padding(txt.isEmpty ? .all : [.top, .leading, .trailing])
+                    .multilineTextAlignment(.center)
+                
+                if !txt.isEmpty {
+                    Button(action: { notification(self.txt) }) {
+                        Text("Remind me!")
+                            .padding()
+                    }
+                    .transition(AnyTransition.offset()
+                    .combined(with: .opacity))
+                }
+                
+            }
+            .frame(height: 0.0)
+            
+            Spacer()
+            Spacer()
+            Spacer()
+            
+        }
+        
     }
 }
 
